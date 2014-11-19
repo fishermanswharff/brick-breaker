8f1a818c6e0fcf0d51d76802d6965f0b9b945882
fishermanswharff <fishermanswharff@github.com>
2014-11-19 10:56:48 -0500

    breaking bricks

--- deploy/js/main.js
+++ deploy/js/main.js
@@ -1,6 +1,8 @@
 (function(){
 	
 	var trace = function(){ for(var i = 0; i < arguments.length; i++){ console.log(arguments[i]); } };
+	var paddleLeft = false;
+	var paddleRight = false;
 	/**
 	*  The main game class
 	*  @class  BrickBreaker 
@@ -29,10 +31,13 @@
 		this.scoreText = null;
 		this.livesText = null;
 
-		// 
+		// new properties that I've added.
 		this.xDirection = 2;
-		this.yDirection = 5;
+		this.yDirection = 3;
 		this.easing = 7;
+		this.isPlaying = false;
+		this.bricks = [];
+
 		
 		// add the container to the stage
 		this.stage.addChild(this.gameContainer);
@@ -72,49 +77,78 @@
 		requestAnimFrame(this.animate);
 		this.renderer.render(this.stage);
 
-		// get the ball from the gameContainer
-		// must be better way than by index????
-		var ball = this.gameContainer.getChildAt(56);
-		var paddle = this.gameContainer.getChildAt(55);
+		if(this.isPlaying){
+			// get the ball from the gameContainer
+			// must be better way than by index????
+			// var ball = this.gameContainer.getChildAt(56);
+			// var paddle = this.gameContainer.getChildAt(55);	
+			
+			// logic for ball bounces off the walls
+			if(this.ball.position.x <= 0){
+				this.xDirection *= -1;
+			} else if(this.ball.position.x >= 640 - this.ball.width) {
+				this.xDirection *= -1;
+			}
 
-		// logic for ball bounces off the walls
-		if(ball.position.x <= 0){
-			this.xDirection *= -1;
-		} else if(ball.position.x >= 640 - ball.width) {
-			this.xDirection *= -1;
-		}
-
-		// logic for ball bouncing off the roof
-		if(ball.position.y >= 480){
-			// session ends
-			// this.initGameState();
-			// this.livesRemaining--;
-		} else if(ball.position.y <= 0) {
-			this.yDirection *= -1;
-		}
-
-		// trace(ball.position.x, ball.position.y);
-		ball.position.y -= this.yDirection;
-		ball.position.x += this.xDirection;
-
-		var xdist = ball.position.x - paddle.position.x;
-		if(xdist > -paddle.width/2 && xdist < paddle.width/2){
-			var ydist = paddle.position.y - ball.position.y;
-			if(ydist > - paddle.height/2 && ydist < paddle.height/2){
+			// logic for this.ball bouncing off the roof
+			if(this.ball.position.y >= 480){
+				// session ends
+				// this.initGameState();
+				// this.livesRemaining--;
+			} else if(this.ball.position.y <= 0) {
 				this.yDirection *= -1;
-				this.checkHitLocation(ball,paddle);
-				// trace('hit code!!!');
+			}
+
+			// trace(this.ball.position.x, this.ball.position.y);
+			this.ball.position.y -= this.yDirection;
+			this.ball.position.x += this.xDirection;
+
+			// if the ball hits the paddle
+			var xdist = this.ball.position.x - this.paddle.position.x;
+			if(xdist > -this.paddle.width/2 && xdist < this.paddle.width/2){
+				var ydist = this.paddle.position.y - this.ball.position.y;
+				if(ydist > - this.paddle.height/2 && ydist < this.paddle.height/2){
+					this.yDirection *= -1;
+					this.checkHitLocation(this.ball,this.paddle);
+					// trace('hit code!!!');
+				}
+			}
+
+			// if the ball hits a brick
+			for(var i = 0; i < this.bricks.length; i++){
+				var brick = this.bricks[i];
+				var xcol = brick.position.x - this.ball.position.x;
+				if(xcol > -brick.width/2 && xcol < brick.width/2){
+					var ycol = brick.position.y - this.ball.position.y;
+					if(ycol > -brick.height/2 && ycol < brick.height/2){
+						// this.bricks.splice(brick[i], 1);
+						this.yDirection *= -1;
+						this.brickContainer.removeChild(brick);
+						this.brickContainer.removeStageReference(brick);
+						// trace(brick);
+					}
+				}
+			}
+
+			// if user presses left arrow
+			if(paddleLeft){
+				this.paddle.position.x -= 5;
+			}
+
+			// if user presses right arrow
+			if(paddleRight){
+				this.paddle.position.x += 5;
 			}
 		}
-		
 	};
 
+	// checking hit location on the paddle to adjust for ball trajectory
 	p.checkHitLocation = function(ball,paddle){
 		var hitPercent, ballPosition;
 		ballPosition = ball.position.x - paddle.position.x;
 		hitPercent = (ballPosition / (paddle.width - ball.width)) - .5;
 		this.xDirection = hitPercent * 10;
-		this.yDirection *= 1.05;
+		this.yDirection *= 1.03;
 	};
 
 	/**
@@ -153,6 +187,8 @@
 	*/
 	p.initGameState = function()
 	{
+		this.isPlaying = true;
+		
 		// add UI
 		//basic grey background shape
 		this.uiHolder = new PIXI.Graphics();
@@ -177,6 +213,7 @@
 
 		//build a layout of bricks
 		//Note: This is placeholder code
+		this.brickContainer = new PIXI.DisplayObjectContainer();
 		for (var col=0;col < 9;col++)
 		{
 			for (var row=0;row < 6;row++)
@@ -184,9 +221,15 @@
 				var brick = PIXI.Sprite.fromFrame("Brick000" + Math.floor(Math.random() * 4));
 				brick.position.x = col * (brick.texture.width + 1);
 				brick.position.y = row * (brick.texture.height + 1);
-				this.gameContainer.addChild(brick);
+				brick.anchor.x = 0.5;
+				brick.anchor.y = 0.5;
+				this.brickContainer.position.x = 0;
+				this.brickContainer.position.y = 0;
+				this.brickContainer.addChild(brick);
+				this.bricks.push(brick);
 			}
 		}
+		this.gameContainer.addChild(this.brickContainer);
 
 		//add the ball and paddle
 		var paddle = PIXI.Sprite.fromFrame("Paddle0000");
@@ -195,6 +238,7 @@
 		paddle.anchor.x = 0.5;
 		paddle.anchor.y = 0.5;
 		this.gameContainer.addChild(paddle);
+		this.paddle = paddle;
 
 		var ball = PIXI.Sprite.fromFrame("Ball0000");
 		ball.position.x = 320 - Math.round(ball.texture.width/2);
@@ -202,35 +246,35 @@
 		ball.anchor.x = 0.5;
 		ball.anchor.y = 0.5;
 		this.gameContainer.addChild(ball);
+		this.ball = ball;
 		
-		// listen for keyboard events, pass in `this` to maintain scope of the pixi object
-		this.addListeners(this);
+		this.addKeyListeners();
 	};
 
-	p.addListeners = function(object){
+	p.addKeyListeners = function(){
+		// trace(object,this,document);
 		document.addEventListener('keydown',function(evt){
 			switch(evt.which){
 				case 37:
-					// ew
-					BrickBreaker.prototype.leftKeyPressed(object.gameContainer.getChildAt(55));
+					paddleLeft = true;
 					break;
 				case 39:
-					// ew
-					BrickBreaker.prototype.rightKeyPressed(object.gameContainer.getChildAt(55));
+					paddleRight = true;
 					break;
 			}
 		});
+		document.addEventListener('keyup',function(evt){
+			switch(evt.which){
+				case 37:
+					paddleLeft = false;
+					break;
+				case 39:
+					paddleRight = false;
+					break;
+			}
+		});
+		
 	};
-
-
-	p.leftKeyPressed = function(object){
-		object.position.x -= 12;
-	};
-
-	p.rightKeyPressed = function(object){
-		object.position.x += 12;
-	};
-	
 
 	/**
 	*  Leave the game state
